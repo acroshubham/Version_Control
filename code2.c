5c5c0b72475d0fdbf4bf331f99d2620a32ce7f0c
acroshubham <acroshubham12@gmail.com>
2023-06-24 22:55:01 +0530

    adding another file

---
+++ code2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("Enter two digits");
+    int a,b;
+    scanf("%d %d", &a, &b);
+    int c=a-b;
+    printf("Substraction of two number is %d",c);
+
+}
