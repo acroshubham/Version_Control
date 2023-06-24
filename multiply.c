22f5192e0bd1a95d7e25a92689381f1c3253cef5
acroshubham <acroshubham12@gmail.com>
2023-06-24 23:26:26 +0530

    This is the feature file

---
+++ multiply.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+ int a,b;
+ printf("Enter two digits");
+ scanf("%d %d", &a, &b);
+ int c=a*b;
+ printf("The mutiplication of two digits are: %d", c);
+return 0;
+}
