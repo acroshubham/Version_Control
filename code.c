7fef71f0f30438239ba2c5a341fbfbf59a879791
acroshubham <acroshubham12@gmail.com>
2023-06-24 17:10:31 +0530

    initial commit

---
+++ code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    printf("\nEner two numbers:");
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    int c=a+b;
+    printf("Sum of a and b is: %d", c);
+    return 0;
+}
